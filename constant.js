810d9656e68c82e2ad613f1fdb2e7eed10610a4c
Matei-thecoder <82590265+Matei-thecoder@users.noreply.github.com>
2022-06-23 17:43:42 +0300

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,25 @@
+const CONSTANT  = {
+    UNASSIGNED : 0,
+    GRID_SIZE:9,
+    BOX_SIZE:3,
+    NUMBERS :[1,2,3,4,5,6,7,8,9],
+    LEVEL_NAME :[
+        'Easy',
+        'Medium',
+        'Hard',
+        'Very Hard',
+        'Insane',
+        'Inhuman'
+    ],
+    LEVEL:[1,1,47,56,65,74],
+    //29,38,47,56,65,74
+    LEVELNAMEDB :[
+        'easy',
+        'medium',
+        'hard',
+        'veryhard',
+        'insane',
+        'inhuman'
+    ]
+
+}
